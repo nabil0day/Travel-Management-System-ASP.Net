3642af13973762bd941db8b0da022d6cd337b2d0
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2023-12-29 12:43:59 +0600

    DAL BLL ADDED

---
+++ BLL/DTOs/BookingDTO.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class BookingDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Booking_Title { get; set; }
+        [Required]
+        public string Booking_Descripion { get; set; }
+        [Required]
+        public string BookedBy { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+
+    }
+}
