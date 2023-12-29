c35493b4052a101fc600c463ce417b4f8ad9ad65
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2023-12-29 12:41:28 +0600

    Application Layer Added

---
+++ TravelManagementSystem/Controllers/TravelController.cs
@@ -0,0 +1,46 @@
+﻿using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace TravelManagementSystem.Controllers
+{
+    public class TravelController : ApiController
+    {
+        [HttpGet]
+        [Route("api/bookings")]
+
+        public HttpResponseMessage Bookings()
+        {
+            try
+            {
+                var data = BookingService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+        [HttpGet]
+        [Route("api/bookings/{id}/suggestions")]
+        public HttpResponseMessage BookingSuggestions(int id)
+        {
+            try
+            {
+                var data = BookingService.GetwithSuggestions(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+
+
+    }
+}
